67693bd99497649a8454fd5116c105ea440d0a09
duchieu23 <hieutd2310@gmail.com>
2021-04-15 12:34:27 +0700

    makeGame

---
+++ game.js
@@ -0,0 +1,80 @@
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var start = false;
+
+$(document).on("keypress",function(){
+  if(!start){
+    nextSequence();
+    start = true;
+  }
+});
+
+$(".btn").click(function(event){
+  userClickedPattern.push(event.target.id);
+  makeSound(event.target.id);
+  animatePress(event.target.id);
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel){
+  if(userClickedPattern[currentLevel]===gamePattern[currentLevel]){
+    console.log("success");
+    if(userClickedPattern.length === gamePattern.length){
+      console.log("Next sequence");
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  }
+  else{
+    console.log("Wrong");
+    makeSound("wrong");
+    $("#level-title").text("Ban la Bo, Nhap 1 phim bat ki de choi lai");
+    $("body").addClass("game-over");
+    setTimeout(function(){
+      $("body").removeClass("game-over");
+    }, 300);
+    gameOver();
+  }
+}
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("LEVEL "+level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  setTimeout(function(){
+    makeSound(randomChosenColour);
+    $("#" + randomChosenColour).fadeOut(50).fadeIn(50);
+  },200); 
+}
+
+function animatePress(currentColor){
+  $("#"+currentColor).addClass("pressed");
+  setTimeout(function(){
+    $("#"+currentColor).removeClass("pressed");
+  }, 100); 
+}
+function makeSound(newSound){
+  var audio = new Audio("sounds/"+newSound+".mp3");
+  audio.play();
+}
+function gameOver(){
+  level = 0;
+  gamePattern = [];
+  userClickedPattern = [];
+  start = false;
+}
+
+
+
+
+
+
+
